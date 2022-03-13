ba66e5c3e8b83d233110f07151a73f23406b479e
Mondido <70878570+DeinyRhed@users.noreply.github.com>
2022-03-13 13:27:25 +0800

    updating

--- dennis_lechon_crm/lib/screens/signin_screen.dart
+++ dennis_lechon_crm/lib/screens/signin_screen.dart
@@ -1,4 +1,5 @@
 //import 'package:dennis_lechon_crm/utils/color_utils.dart';
+import 'package:dennis_lechon_crm/utils/color_utils.dart';
 import 'package:dennis_lechon_crm/widgets/reusable_widget.dart';
 import "package:flutter/material.dart";
 
@@ -10,9 +11,12 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: hexStringToColor("D3231E"),
         body: Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
@@ -21,10 +25,20 @@ class _SignInState extends State<SignIn> {
                   padding: EdgeInsets.fromLTRB(
                       20, MediaQuery.of(context).size.height * 0.2, 20, 0),
                   child: Column(children: <Widget>[
-                    logoWidget("3M_Dennis_logo.jpg"),
+                    logoWidget("assets/image/dennislogo.png"),
                     const SizedBox(
                       height: 30,
-                    )
+                    ),
+                    reusableTextField("Enter UserName", Icons.person_outline,
+                        false, _emailTextController),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    reusableTextField("Enter Password", Icons.lock_outline,
+                        true, _passwordTextController),
+                    const SizedBox(
+                      height: 5,
+                    ),
                   ])),
             )));
   }
