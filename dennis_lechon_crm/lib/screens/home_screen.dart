5b4ebae3e7d664494cace3dc265bc2546c8ab554
Emmalyn Acha <ecacha@up.edu.ph>
2022-03-15 03:28:30 +0800

    homepage v2

--- dennis_lechon_crm/lib/screens/home_screen.dart
+++ dennis_lechon_crm/lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:dennis_lechon_crm/screens/homebuttons/calendar_screen.dart';
+import 'package:dennis_lechon_crm/screens/homebuttons/customer_screen.dart';
+import 'package:dennis_lechon_crm/screens/homebuttons/orderList_screen.dart';
 import 'package:dennis_lechon_crm/screens/signin_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,34 +22,70 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton.icon(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.amber,
-                onPrimary: Colors.black,
-                minimumSize: Size(170, 70),
-              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CustomerScreen()));
+              },
               icon: Icon(Icons.person),
-              label: Text("Customer List"),
+              label: Text(
+                "Customer List",
+                style: GoogleFonts.oxygen(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFFF1A22C),
+                onPrimary: Colors.white,
+                elevation: 12,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+                minimumSize: Size(230, 90),
+              ),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => OrderListScreen()));
+              },
               style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                onPrimary: Colors.black,
-                minimumSize: Size(170, 70),
+                primary: Color(0xFFD3231E),
+                onPrimary: Colors.white,
+                elevation: 12,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+                minimumSize: Size(230, 90),
               ),
               icon: Icon(Icons.list),
-              label: Text("Order List"),
+              label: Text(
+                "Order List",
+                style: GoogleFonts.oxygen(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CalendarScreen()));
+              },
               style: ElevatedButton.styleFrom(
-                primary: Colors.lightBlue,
-                onPrimary: Colors.black,
-                minimumSize: Size(170, 70),
+                primary: Color(0xFF2A87BB),
+                onPrimary: Colors.white,
+                elevation: 12,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(60.0)),
+                minimumSize: Size(230, 90),
               ),
               icon: Icon(Icons.calendar_month),
-              label: Text("Calendar"),
+              label: Text(
+                "Calendar",
+                style: GoogleFonts.oxygen(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             ElevatedButton(
               child: Text("Logout"),
