0e36fdd0add51dd8f339be6f0d619f1a958cb83d
Mondido <70878570+DeinyRhed@users.noreply.github.com>
2022-03-17 02:35:45 +0800

    Revert "Merge branch 'sprint-1' of https://github.com/melzar-jan-chico-UP/dennis-lechon-crm into sprint-1"
    This reverts commit bbfb33154740e193e88488af07e4f9315e44af01, reversing
    changes made to 37ff928f7a2287566a7c9f7bad44fa742aa68958.

--- dennis_lechon_crm/lib/screens/signin_screen.dart
+++ dennis_lechon_crm/lib/screens/signin_screen.dart
@@ -5,7 +5,6 @@ import 'package:dennis_lechon_crm/widgets/reusable_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'home_screen.dart';
-import 'package:dennis_lechon_crm/widgets/loading.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -17,95 +16,84 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
-  bool loading = false;
-
   @override
   Widget build(BuildContext context) {
-    return loading
-        ? Loading()
-        : Scaffold(
-            backgroundColor: Color.fromARGB(255, 185, 17, 5),
-            body: Stack(children: <Widget>[
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    //Image needs to be changed to avoid stretching
-                    image: AssetImage("images/lechon_loginpage.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 185, 17, 5),
+        body: Stack(children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                //Image needs to be changed to avoid stretching
+                image: AssetImage("images/lechon_loginpage.png"),
+                fit: BoxFit.cover,
               ),
-              SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: SingleChildScrollView(
-                    child: Padding(
-                        padding: EdgeInsets.fromLTRB(20,
-                            MediaQuery.of(context).size.height * 0.1, 20, 0),
-                        child: Column(children: <Widget>[
-                          Image.asset(
-                            "assets/images/dennislogo.png",
-                            fit: BoxFit.fitWidth,
-                            width: 240,
-                            height: 240,
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          reusableTextField("Enter Email", Icons.person_outline,
-                              false, _emailTextController),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          reusableTextField(
-                              "Enter Password",
-                              Icons.lock_outline,
-                              true,
-                              _passwordTextController),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          forgotPassword(context),
-                          firebaseUIButton(context, "Sign In", () {
-                            FirebaseAuth.instance
-                                .signInWithEmailAndPassword(
-                                    email: _emailTextController.text,
-                                    password: _passwordTextController.text)
-                                .then((value) async {
-                              setState(() => loading = true);
-                              await Future.delayed(Duration(seconds: 1));
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const HomeScreen()));
-                            }).onError((error, stackTrace) {
-                              loading = false;
-                              debugPrint("Error ${error.toString()}");
+            ),
+          ),
+          SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(
+                        20, MediaQuery.of(context).size.height * 0.1, 20, 0),
+                    child: Column(children: <Widget>[
+                      Image.asset(
+                        "assets/images/dennislogo.png",
+                        fit: BoxFit.fitWidth,
+                        width: 240,
+                        height: 240,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      reusableTextField("Enter Email", Icons.person_outline,
+                          false, _emailTextController),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      reusableTextField("Enter Password", Icons.lock_outline,
+                          true, _passwordTextController),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      forgotPassword(context),
+                      firebaseUIButton(context, "LOG IN", () {
+                        FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: _emailTextController.text,
+                                password: _passwordTextController.text)
+                            .then((value) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomeScreen()));
+                        }).onError((error, stackTrace) {
+                          debugPrint("Error ${error.toString()}");
 
-                              // Error Prompt here
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text("Login Error"),
-                                      content: Text(error.toString()),
-                                      actions: [
-                                        TextButton(
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                            child: const Text("OK"))
-                                      ],
-                                    );
-                                  });
-                              // Ends here
-                            });
-                          }),
-                          //signUpOption(),
-                        ])),
-                  ))
-            ]));
+                          // Error Prompt here
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text("Login Error"),
+                                  content: Text(error.toString()),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text("OK"))
+                                  ],
+                                );
+                              });
+                          // Ends here
+                        });
+                      }),
+                      //signUpOption(),
+                    ])),
+              ))
+        ]));
   }
 
   Row signUpOption() {
