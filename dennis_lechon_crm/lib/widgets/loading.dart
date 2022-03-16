616097d2d5e8634c2cc294c595d0507c24dd86ac
Mondido <70878570+DeinyRhed@users.noreply.github.com>
2022-03-17 03:13:44 +0800

    fixed errors

--- dennis_lechon_crm/lib/widgets/loading.dart
+++ dennis_lechon_crm/lib/widgets/loading.dart
@@ -1,4 +1,3 @@
-import 'package:dennis_lechon_crm/utils/color_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -6,7 +5,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: hexStringToColor("D3231E"),
+      color: Color.fromARGB(255, 185, 17, 5),
       child: Center(
         child: SpinKitFadingCircle(
           color: Colors.white,
