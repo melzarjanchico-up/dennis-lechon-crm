bb4fd52a7e4ad313bc57d33d26fe7e4a550c4346
Emmalyn Acha <ecacha@up.edu.ph>
2022-03-22 04:26:59 +0800

    customer_screen v2

--- lib/screens/homebuttons/customer_screen.dart
+++ lib/screens/homebuttons/customer_screen.dart
@@ -1,10 +1,34 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'package:flutter/cupertino.dart';
+import 'package:dennis_lechon_crm/screens/login_screens/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:dennis_lechon_crm/widgets/loading.dart';
+import 'package:dennis_lechon_crm/widgets/loading.dart';
+
+final List<String> entries = <String>[
+  'Hot',
+  'Warm',
+  'Cold',
+  'Kakamfenk',
+  'BabyM',
+  'Dead',
+  'Fucker',
+  'Fuckity',
+  'FrFrNglIstg'
+];
+List<Color> colors = [
+  Color(0xFFD3231E),
+  Color(0xFFF1A22C),
+  Color(0xFF2A87BB),
+  Color.fromARGB(255, 218, 40, 164),
+  Color.fromARGB(255, 24, 212, 65),
+  Color.fromARGB(255, 8, 8, 8),
+  Color.fromARGB(255, 187, 42, 102),
+  Color.fromARGB(255, 86, 42, 187),
+  Color.fromARGB(255, 51, 15, 180)
+];
 
 // firestore test shiz
 //import '../../services/test_database.dart';
@@ -16,79 +40,37 @@ class CustomerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "route",
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFFF1A22C),
-          title: const Text("Customer List"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () async {},
-                icon: const Icon(Icons.fireplace_rounded),
-                label: Text(
-                  "Hot",
-                  style: GoogleFonts.oxygen(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFFD3231E),
-                  onPrimary: Colors.white,
-                  elevation: 12,
-                  minimumSize: const Size(400, 75),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF1A22C),
+        title: const Text("Customer List"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ElevatedButton.icon(
+              onPressed: () async {},
+              icon: const Icon(Icons.fireplace_rounded),
+              label: Text(
+                " ${entries[index]} ",
+                style: GoogleFonts.oxygen(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              ElevatedButton.icon(
-                onPressed: () async {},
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFFF1A22C),
-                  onPrimary: Colors.white,
-                  elevation: 12,
-                  minimumSize: const Size(400, 75),
-                ),
-                icon: const Icon(Icons.hot_tub),
-                label: Text(
-                  "Warm",
-                  style: GoogleFonts.oxygen(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+              style: ElevatedButton.styleFrom(
+                primary: colors[index],
+                onPrimary: Colors.white,
+                elevation: 12,
+                minimumSize: const Size(400, 75),
               ),
-              ElevatedButton.icon(
-                onPressed: () async {},
-                style: ElevatedButton.styleFrom(
-                  primary: const Color(0xFF2A87BB),
-                  onPrimary: Colors.white,
-                  elevation: 12,
-                  minimumSize: const Size(400, 75),
-                ),
-                icon: const Icon(Icons.ac_unit),
-                label: Text(
-                  "Cold",
-                  style: GoogleFonts.oxygen(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              RaisedButton(
-                child: const Text("Back"),
-                color: const Color(0xFFF1A22C),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-          //body: Center(
-          //child:
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
         ),
       ),
     );
