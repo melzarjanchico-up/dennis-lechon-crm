a3779e8581984a562a5916e5913ae70748d8971e
Mondido <70878570+DeinyRhed@users.noreply.github.com>
2022-03-23 02:45:56 +0800

    fixed errors

--- lib/screens/homebuttons/customer_screen.dart
+++ lib/screens/homebuttons/customer_screen.dart
@@ -26,45 +26,44 @@ class CustomerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //ERROR: loading time still needs work because null exception will exist when data has not been fetched yet
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFFF1A22C),
-          title: const Text("Customer List"),
-          centerTitle: true,
-        ),
-        //listview.separate will be added for spacing
-        body: Stack(children: <Widget>[
-          Center(
-            child: StreamBuilder(
-              //to be separated
-              stream: FirebaseFirestore.instance.collection('tags').snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                return ListView(
-                  children: snapshot.data!.docs.map((tags) {
-                    return ElevatedButton.icon(
-                      onPressed: () async {
-                        //insert method
-                      },
-                      icon: const Icon(Icons.fireplace_rounded),
-                      label: Text(
-                        tags['name'],
-                        style: GoogleFonts.oxygen(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: colors[3],
-                        onPrimary: Colors.white,
-                        elevation: 12,
-                        minimumSize: const Size(400, 75),
-                      ),
-                    );
-                  }).toList(),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF1A22C),
+        title: const Text("Customer List"),
+        centerTitle: true,
+      ),
+      //listview.separate will be added for spacing
+      body: Center(
+        child: StreamBuilder(
+          //to be separated
+          stream: FirebaseFirestore.instance.collection('tags').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            return ListView(
+              children: snapshot.data!.docs.map((tags) {
+                return ElevatedButton.icon(
+                  onPressed: () async {
+                    //insert method
+                  },
+                  icon: const Icon(Icons.fireplace_rounded),
+                  label: Text(
+                    tags['name'],
+                    style: GoogleFonts.oxygen(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: colors[3],
+                    onPrimary: Colors.white,
+                    elevation: 12,
+                    minimumSize: const Size(400, 75),
+                  ),
                 );
-              },
-            ),
-          ),
-        ]));
+              }).toList(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
