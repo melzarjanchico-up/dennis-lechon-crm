6accd067b9f77bedad665da775bd0ebf2d15fc6a
vro <91467368+vergeljabello@users.noreply.github.com>
2022-03-24 01:58:35 +0800

    updated customer info

--- lib/screens/customer_screen/customer_info.dart
+++ lib/screens/customer_screen/customer_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class CustomerInfo extends StatefulWidget {
   const CustomerInfo({Key? key}) : super(key: key);
@@ -10,6 +11,168 @@ class CustomerInfo extends StatefulWidget {
 class _CustomerInfoState extends State<CustomerInfo> {
   @override
   Widget build(BuildContext context) {
-    return const Text("FORKING");
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Melzar Batig Nawng",
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "09123567890",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Lapu-Lapu City",
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "First Ordered",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "Dec 2018",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Last Ordered",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "Dec 2020",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Tags",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "Hot",
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                // gi ontap para sa future na edit notes :)
+                debugPrint('Card tapped.');
+              },
+              child: const SizedBox(
+                width: 300,
+                height: 150,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'NOTES',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Card(
+            child: InkWell(
+              splashColor: Colors.blue.withAlpha(30),
+              onTap: () {
+                // gi ontap para sa future na edit notes :)
+                debugPrint('Card tapped.');
+              },
+              child: const SizedBox(
+                width: 200,
+                height: 150,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Orders',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
