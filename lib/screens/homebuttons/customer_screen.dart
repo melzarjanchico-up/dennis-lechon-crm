e4d2f23945496cf9d8d44ac672cebc23dea70327
Emmalyn Acha <ecacha@up.edu.ph>
2022-03-30 02:51:45 +0800

    addcustomer_popup w/ search customer

--- lib/screens/homebuttons/customer_screen.dart
+++ lib/screens/homebuttons/customer_screen.dart
@@ -10,6 +10,7 @@ class CustomerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return StreamProvider<List<Customer>>.value(
       value: CustomerService().customers,
       initialData: const [],
@@ -29,33 +30,171 @@ class CustomerScreen extends StatelessWidget {
                 icon: const Icon(Icons.search),
               ),
             ],
-            /* code ni ni melzzar katong plus button sa top right na mu add ni ariana @emmalynboang
-            actions: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: GestureDetector(
-                    onTap: () async {
-                      await CustomerService().addCustomer(
-                          "Ariana",
-                          "Paquibot",
-                          "Grande",
-                          "6015",
-                          "Basak",
-                          "Lapu-Lapu",
-                          "123",
-                          "456",
-                          "Heehee",
-                          "Warm",
-                          2,
-                          "Color(0xFFF1A22C)");
-                    },
-                    child: const Icon(
-                      Icons.search,
-                      size: 26.0,
-                    ),
-                  ))
-            ],
-            */
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            onPressed: () async {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      contentPadding: const EdgeInsets.only(top: 0.0),
+                      content: Stack(
+                        children: <Widget>[
+                          Container(
+                            width: double.infinity,
+                            height: 45,
+                            alignment: Alignment.topCenter,
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFD3231E),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(25),
+                                topRight: Radius.circular(25),
+                              ),
+                            ),
+                            child: const Align(
+                              alignment: Alignment.center,
+                              // ignore: unnecessary_const
+                              child: const Text(
+                                "Add Customer",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    letterSpacing: 1.0,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            right: -40.0,
+                            top: -40.0,
+                            child: InkResponse(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const CircleAvatar(
+                                child: Icon(Icons.close),
+                                backgroundColor: Colors.red,
+                              ),
+                            ),
+                          ),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 55, 15, 0),
+                                  child: imageProfile(),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 0, 15, 6),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: 'Customer Name',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 6, 15, 6),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: 'Contact Number',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 6, 15, 6),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: 'Address',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 6, 15, 6),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: 'Birthday',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 6, 15, 6),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      labelText: 'Age',
+                                    ),
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.fromLTRB(15, 6, 15, 6),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: 'Tag',
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                  child: ElevatedButton(
+                                    child: const Text("     CREATE     "),
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        _formKey.currentState!.save();
+                                      } else {
+                                        return null;
+                                      }
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      primary: const Color(0xFFD3231E),
+                                      onPrimary: Colors.white,
+                                      elevation: 5,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0)),
+                                      minimumSize: const Size(75, 40),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+              /*await CustomerService().addCustomer(
+                  "Ariana",
+                  "Paquibot",
+                  "Grande",
+                  "6015",
+                  "Basak",
+                  "Lapu-Lapu",
+                  "123",
+                  "456",
+                  "Heehee",
+                  "Warm",
+                  2,
+                  "Color(0xFFF1A22C)");
+              */
+            },
+            icon: const Icon(
+              Icons.add_circle_outline,
+              size: 26.0,
+            ),
+            backgroundColor: const Color(0xFFF1A22C),
+            label: const Text('Add Customer'),
           ),
           body: StreamBuilder<List<Customer>>(
             stream: CustomerService().customers,
@@ -81,6 +220,25 @@ class CustomerScreen extends StatelessWidget {
           )),
     );
   }
+}
+
+Widget imageProfile() {
+  return Center(
+    child: Stack(children: <Widget>[
+      const CircleAvatar(
+        radius: 40.0,
+        backgroundImage: AssetImage('assets/images/user.png'),
+      ),
+      Positioned(
+          bottom: 20.0,
+          right: 20.0,
+          child: Icon(
+            Icons.camera_alt,
+            color: Colors.white.withOpacity(0.5),
+            size: 33.0,
+          )),
+    ]),
+  );
 }
 
 //link sa "inspiration" na video [https://www.youtube.com/watch?v=KF1KMfQOpjM&list=LL&index=1] hehe
