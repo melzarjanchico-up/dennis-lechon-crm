0c30143784c5cc97c12021d954086ab26701484c
Emmalyn Acha <ecacha@up.edu.ph>
2022-04-12 02:04:19 +0800

    added display_order popup on order_screen

--- lib/screens/homebuttons/order_screen.dart
+++ lib/screens/homebuttons/order_screen.dart
@@ -38,10 +38,288 @@ class OrderListScreen extends StatelessWidget {
             GestureDetector(
               onTap: () {
                 // sa customer list ni siya na code po huehue
-                //Navigator.push(
-                //    context,
-                //     MaterialPageRoute(
-                //         builder: (context) => CustomerInfo(customer: customer)));
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                        ),
+                        contentPadding: const EdgeInsets.only(top: 0.0),
+                        content: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Stack(
+                                children: <Widget>[
+                                  const Positioned(
+                                    top: 10,
+                                    right: 22,
+                                    child: Text(
+                                      "#2891382 ",
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 209, 205, 205),
+                                          letterSpacing: 1.0,
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: double.infinity,
+                                    height: 105,
+                                    alignment: Alignment.center,
+                                    decoration: const BoxDecoration(
+                                      color: Color(0xFFD3231E),
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(25),
+                                        topRight: Radius.circular(25),
+                                      ),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        const SizedBox(height: 15),
+                                        RichText(
+                                          text: const TextSpan(
+                                            text: "Order ",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                letterSpacing: 1.0,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w900),
+                                            children: [
+                                              TextSpan(
+                                                text: "#28391829 ",
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  letterSpacing: 1.0,
+                                                  fontSize: 13,
+                                                  fontStyle: FontStyle.italic,
+                                                  fontWeight: FontWeight.w300,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10),
+                                        const Text(
+                                          'To be delivered on:     \u{1F4D9}   ',
+                                          style: TextStyle(
+                                              fontSize: 13.0,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w100),
+                                        ),
+                                        const SizedBox(height: 10),
+                                        RichText(
+                                          text: const TextSpan(
+                                            text: '\u{26A1} RUSH    ',
+                                            style: TextStyle(
+                                                fontSize: 13.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w900),
+                                            children: [
+                                              TextSpan(
+                                                text: '\u{2705} ACTIVE',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                  letterSpacing: 1.0,
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.w900,
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Positioned(
+                                    right: 3.0,
+                                    top: 5.0,
+                                    child: InkResponse(
+                                      onTap: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const CircleAvatar(
+                                        radius: 17,
+                                        backgroundColor:
+                                            Color.fromARGB(255, 173, 23, 18),
+                                        child: Icon(
+                                          Icons.close,
+                                          color: Colors.white,
+                                          size: 23.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: const [
+                                      SizedBox(height: 120),
+                                      Align(
+                                        alignment: Alignment.topCenter,
+                                        child: Text(
+                                          'Name:',
+                                          style: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 10.0,
+                                              fontWeight: FontWeight.w300),
+                                        ),
+                                      ),
+                                      SizedBox(width: 0, height: 0),
+                                      Text(
+                                        'Emma Axe',
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            color: Colors.black,
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      SizedBox(width: 0, height: 0),
+                                      Text(
+                                        'Contact No.',
+                                        style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 10.0,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      SizedBox(width: 0, height: 0),
+                                      Text(
+                                        '0923101293',
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            color: Colors.black,
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      SizedBox(width: 0, height: 0),
+                                      Text(
+                                        'Address',
+                                        style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 10.0,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      Text(
+                                        'Minglanilla, Cebu',
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            color: Colors.black,
+                                            letterSpacing: 2.0,
+                                            fontWeight: FontWeight.w300),
+                                      ),
+                                      SizedBox(height: 20),
+                                      Text(
+                                        'Order Details',
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                      SizedBox(height: 50),
+                                    ],
+                                  ),
+                                  Positioned(
+                                    top: 500,
+                                    left: 35,
+                                    child: RichText(
+                                      text: const TextSpan(
+                                        text: 'Total',
+                                        style: TextStyle(
+                                            fontSize: 18.0,
+                                            color:
+                                                Color.fromARGB(255, 29, 29, 29),
+                                            fontWeight: FontWeight.w900),
+                                        children: [
+                                          TextSpan(
+                                            text: '  (5 items)  ',
+                                            style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 80, 79, 79),
+                                              letterSpacing: 1.0,
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.w900,
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text: '                  Php 5,000',
+                                            style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 80, 79, 79),
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w800,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Card(
+                                    color: Color.fromARGB(255, 243, 243, 243),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                    ),
+                                    elevation: 3,
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 20.0, vertical: 265.0),
+                                    child: Padding(
+                                        padding: const EdgeInsets.all(100),
+                                        child: Column(children: const [
+                                          Text(
+                                            'Lechon Spicy',
+                                            style: TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 39, 39, 39),
+                                                fontSize: 20.0,
+                                                fontWeight: FontWeight.w500),
+                                          )
+                                        ])),
+                                  ),
+                                  Positioned(
+                                    bottom: 175,
+                                    right: 12,
+                                    child: Row(
+                                      children: [
+                                        RaisedButton(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(5.0),
+                                          ),
+                                          child: const Text('Edit',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                              )),
+                                          color: const Color(0xFFD3231E),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                        const SizedBox(width: 75),
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            primary: const Color(0xFFD3231E),
+                                            onPrimary: Colors.white,
+                                            elevation: 5,
+                                            shape: const CircleBorder(),
+                                            padding: const EdgeInsets.all(15.0),
+                                            minimumSize: const Size(15, 5),
+                                          ),
+                                          child: const Icon(
+                                              Icons.delete_forever_rounded),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
               },
               child: Container(
                 padding: const EdgeInsets.all(10),
