0f52e56e4ff83cef641db73f8163b828b4d022f1
Emmalyn Acha <ecacha@up.edu.ph>
2022-06-14 22:20:43 +0800

    customer dashboard update
    mao ni vorr

--- lib/screens/home_screen_new/components/customer_dashboard.dart
+++ lib/screens/home_screen_new/components/customer_dashboard.dart
@@ -113,6 +113,20 @@ class _CustomerDashState extends State<CustomerDash> {
                             aspectRatio: 2,
                             child: PieChart(
                               PieChartData(
+                                  pieTouchData: PieTouchData(touchCallback:
+                                      (FlTouchEvent event, pieTouchResponse) {
+                                    setState(() {
+                                      if (!event.isInterestedForInteractions ||
+                                          pieTouchResponse == null ||
+                                          pieTouchResponse.touchedSection ==
+                                              null) {
+                                        touchedIndex = -1;
+                                        return;
+                                      }
+                                      touchedIndex = pieTouchResponse
+                                          .touchedSection!.touchedSectionIndex;
+                                    });
+                                  }),
                                   borderData: FlBorderData(
                                     show: false,
                                   ),
